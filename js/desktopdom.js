9259f4ba3c1a6a79de16a6bbce47f58e15fe9eca
pypalkar23 <mandar.palkar94@gmail.com>
2015-02-26 20:19:08 +0530

    after modularizing

---
+++ js/desktopdom.js
@@ -0,0 +1,179 @@
+function createroomdom()
+{
+
+  var main=document.getElementById("main");
+  while(main.firstChild)
+  {
+    //clears the main division before appending anything
+    main.removeChild(main.firstChild);
+  }
+  var body=document.body;
+  
+  //createform
+  var roomform=document.createElement("form");
+  roomform.setAttribute("name","roominput");
+  roomform.id="roomform";
+
+  //paper-toolbar
+  var toolbar=document.createElement("core-toolbar");
+  toolbar.setAttribute("id","top-toolbar");
+  toolbar.setAttribute("class","medium-tall");
+    
+  /*var toolbarbutton=document.createElement("core-icon-button");
+  toolbarbutton.setAttribute("icon","arrow-back");
+  toolbarbutton.setAttribute("class","bottom");
+  //toolbarbutton.setAttribute("on-tap","");*/
+  
+  //text inside paper toolbar
+  var toolbarspan=document.createElement("span");
+  toolbarspan.innerHTML="Create a Room";
+  toolbarspan.setAttribute("class","middle intent");
+  
+
+  var toolbardiv=document.createElement("div");
+  toolbardiv.id="header";
+
+  var bodydiv=document.createElement("div");
+  bodydiv.id="bodydiv";
+
+
+  //toolbar.appendChild(toolbarbutton);
+  toolbar.appendChild(toolbarspan);
+  toolbardiv.appendChild(toolbar);
+
+  var inputdecorator=document.createElement("paper-input-decorator");
+  inputdecorator.setAttribute("label","Room number");
+  inputdecorator.floatingLabel=true;
+
+  var roomno=document.createElement("input");
+  roomno.setAttribute("is","core-input");
+  roomno.setAttribute("type","number");
+  roomno.setAttribute("name","roomno");
+  roomno.setAttribute("readOnly","true");
+  roomno.setAttribute("required","true");
+  roomno.setAttribute("id","roomno");
+
+  var genbutton=document.createElement("paper-button");
+  genbutton.setAttribute("class","colored"); 
+  genbutton.setAttribute("role","button");
+  genbutton.setAttribute("raised","true");
+  genbutton.setAttribute("name","generateroomno");
+  genbutton.setAttribute("onClick","generateRoomNo()");
+  genbutton.id="generateroombutton";
+  genbutton.innerHTML="Generate Room";
+
+  var roombutton=document.createElement("paper-button");
+  roombutton.setAttribute("class","colored");
+  roombutton.setAttribute("role","button");
+  roombutton.setAttribute("raised","true");
+  roombutton.setAttribute("name","createroom");
+  roombutton.setAttribute("onClick","createRoom()");
+  roombutton.id="submitroombutton";
+  roombutton.innerHTML="Create Room";
+
+  var error=document.createElement("P");
+  var newLine=document.createElement("br");
+  error.id="error";
+  inputdecorator.appendChild(roomno);
+  roomform.appendChild(inputdecorator);
+  roomform.appendChild(genbutton);
+  roomform.appendChild(roombutton);
+  bodydiv.appendChild(roomform);
+  roomform.appendChild(error);
+  main.appendChild(toolbardiv);
+  main.appendChild(bodydiv);
+}
+
+function generateRoomNo()
+{
+  var roomno=0;
+  document.getElementById("roomno").focus();
+  //generates 4 digit random no
+  for(var i=0;i<4;i++)
+    roomno+=Math.floor(Math.random()*9+1)*Math.pow(10,i);
+  document.getElementById("roomno").value=roomno;
+  document.getElementById("error").style.display="none";
+}
+
+function showRoomDom(message)
+{
+  var main=document.getElementById("main");
+  while(main.firstChild)
+  {
+    main.removeChild(main.firstChild);
+  }
+
+  var toolbarspan=document.createElement("span");
+  toolbarspan.innerHTML="Join a Room";
+  toolbarspan.setAttribute("class","middle intent");
+  
+  var toolbar=document.createElement("core-toolbar");
+  toolbar.setAttribute("id","top-toolbar");
+  toolbar.setAttribute("class","medium-tall");
+
+  var toolbardiv=document.createElement("div");
+  toolbardiv.id="header";
+
+  toolbar.appendChild(toolbarspan);
+  toolbardiv.appendChild(toolbar);
+
+  var bodydiv=document.createElement("div");
+  bodydiv.id="bodydiv";
+
+  
+  var roomno=document.createElement("p");
+  roomno.id="textroomno";
+  roomno.innerHTML=message;
+ 
+  var gentext=document.createElement("p");
+  gentext.id="roomnotext"
+  
+
+  var roombutton=document.createElement("paper-button");
+  roombutton.setAttribute("class","colored");
+  roombutton.setAttribute("role","button");
+  roombutton.setAttribute("raised","true");
+  roombutton.setAttribute("name","createroom");
+  roombutton.setAttribute("onClick","createroomdom()");
+  roombutton.id="backbutton";
+  roombutton.innerHTML="Create another Room";
+  gentext.innerHTML="Sample Text";
+
+  var spinnerdiv=document.createElement("div");
+  spinnerdiv.id="spinner-div";
+
+  var spinnertext=document.createElement("p");
+  spinnertext.id="spinnertext";
+  spinnertext.innerHTML="Waiting for other peer to connect";
+
+  var spinner=document.createElement("paper-spinner");
+  spinner.id="spinner";
+  spinner.active=true;
+
+  spinnerdiv.appendChild(spinnertext);
+  spinnerdiv.appendChild(spinner)
+
+  main.appendChild(toolbardiv);
+  bodydiv.appendChild(roomno);
+  bodydiv.appendChild(gentext);
+  bodydiv.appendChild(spinnerdiv);
+  bodydiv.appendChild(roombutton);
+  main.appendChild(bodydiv);
+}
+
+function createRoom()
+{
+  roomno=document.getElementById("roomno").value;
+  
+  if(roomno.length===0)
+  {
+    document.getElementById("error").innerHTML="Please generate a room number first";
+  }
+  else
+  {
+    var msg={action:"createroom"};
+    JSON.stringify(msg);
+    sendMessage(msg);//replace this with send message
+    
+  }
+}
---
+++ js/mobiledom.js
@@ -0,0 +1,65 @@
+function createroomdom()
+{
+  var main=document.getElementById("main");
+  
+  while(main.firstChild)
+    main.removeChild(main.firstChild);
+
+  var toolbarspan=document.createElement("span");
+  toolbarspan.innerHTML="Join a Room";
+  toolbarspan.setAttribute("class","middle intent");
+
+  var toolbar=document.createElement("core-toolbar");
+  toolbar.setAttribute("id","top-toolbar");
+  toolbar.flex=true;
+  toolbar.setAttribute("class","medium-tall");
+
+  var toolbardiv=document.createElement("div");
+  toolbardiv.id="header";
+
+  toolbar.appendChild(toolbarspan);
+  toolbardiv.appendChild(toolbar);
+
+  var roomform=document.createElement("form");
+  roomform.setAttribute("name","roomjoin");
+  roomform.id="roomjoin";
+
+  var roominput=document.createElement("paper-input-decorator");
+  roominput.floatingLabel=true;
+  roominput.setAttribute("label","Room number");
+  
+  var coreinput=document.createElement("input");
+  coreinput.setAttribute("is","core-input");
+  coreinput.setAttribute("type","number"); 
+  coreinput.setAttribute("name","roominput"); 
+  coreinput.setAttribute("maxlength",4);
+  coreinput.setAttribute("size",4); 
+  
+  roominput.appendChild(coreinput);
+
+  var joinbutton=document.createElement("paper-button");
+  joinbutton.setAttribute("class","colored");
+  joinbutton.setAttribute("role","button");
+  joinbutton.setAttribute("raised","true");
+  joinbutton.innerHTML="Join Room"; 
+  joinbutton.setAttribute("onClick","checkRoom()");
+
+  var error=document.createElement("P");
+  error.id="error";
+
+  roomform.appendChild(roominput);
+  roomform.appendChild(error);
+  roomform.appendChild(joinbutton);
+  main.appendChild(toolbardiv);
+  main.appendChild(roomform);
+}
+
+function checkRoom()
+{
+  roomno=document.forms["roomjoin"]["roominput"].value;
+  if (roomno<1000 && roomno>9999)
+    document.getElementById("error").innerHTML="Room no must be a 4 digit value";
+  else
+     var message={action:"joinroom"};
+     sendMessage(message);
+}
